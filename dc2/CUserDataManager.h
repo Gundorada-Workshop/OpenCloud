3855e5ac8584f864d52fe15c0f4a9604a37d51e1
raSTARgfx <rastargfx@protonmail.com>
2022-10-28 08:47:36 -0400

    add CUserDataManager stubs

---
+++ dc2/CUserDataManager.h
@@ -0,0 +1,18 @@
+class CUserDataManager
+{
+public:
+  // 0019b160
+  void Initialize();
+
+  // 0019b9a0
+  void JoinPartyMember(int chara);
+
+  // 0019baa0
+  void EnableCharaChange(int chara);
+
+  // 0019c420
+  void SetRoboName(const char* name);
+
+  // 0019c450
+  char* GetRoboNameDefault();
+};
