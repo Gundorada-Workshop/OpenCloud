5f44c27b97f42d920b3471ddb0c1cf0a6da3c30e
raSTARgfx <rastargfx@protonmail.com>
2022-10-28 10:36:51 -0400

    add AxisCalibration

--- dc2/CGamePad.cc
+++ dc2/CGamePad.cc
@@ -1,5 +1,20 @@
 #include "CGamePad.h"
 
+#include <cmath>
+
+// 0014aef0
+static int AxisCalibration(int axis)
+{
+  // dead zone
+  if(std::abs(axis - 128) < 50)
+    return 0;
+
+  if((axis - 128) < 1)
+    return ((axis - 78) * 128) / 78;
+
+  return ((axis - 177) * 128) / 78;
+}
+
 float CGamePad::GetRXf()
 {
   return static_cast<float>(GetRX()) / 128.0f;
