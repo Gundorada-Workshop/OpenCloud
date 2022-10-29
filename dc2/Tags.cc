4da4664a7a1ee9d0be2bf9e66faa97109ae006f9
raSTARgfx <rastargfx@protonmail.com>
2022-10-29 19:38:58 -0400

    move tex_tag to mgCTextureManager

--- dc2/Tags.cc
+++ dc2/Tags.cc
@@ -1,74 +1,5 @@
 #include "Tags.h"
 
-static bool texTEX_ANIME(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texTEX_ANIME_DATA(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texSRC_TEX(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texDEST_TEX(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texSCROLL(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texCLUT_COPY(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texCOLOR(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texALPHA_BLEND(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texALPHA_TEST(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texWAIT(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texTEX_ANIME_DATA_END(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texANIME_END(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-static bool texANIME_BUG_PATCH(SPI_STACK* stack, int)
-{
-  return true;
-}
-
-
-
-
-
 static bool __BUFFER_SIZE(SPI_STACK* stack, int)
 {
   return true;
@@ -628,25 +559,6 @@ static bool dynWIND(SPI_STACK* stack, int)
 {
   return true;
 }
-
-// 003340b0
-const std::array<SPI_TAG_PARAM, 14> tex_tag =
-{
-  "TEX_ANIME",          texTEX_ANIME,
-  "TEX_ANIME_DATA",     texTEX_ANIME_DATA,
-  "SRC_TEX",            texSRC_TEX,
-  "DEST_TEX",           texDEST_TEX,
-  "SCROLL",             texSCROLL,
-  "CLUT_COPY",          texCLUT_COPY,
-  "COLOR",              texCOLOR,
-  "ALPHA_BLEND",        texALPHA_BLEND,
-  "ALPHA_TEST",         texALPHA_TEST,
-  "WAIT",               texWAIT,
-  "TEX_ANIME_DATA_END", texTEX_ANIME_DATA_END,
-  "TEX_ANIME_END",      texANIME_END,
-  "BUG_PATCH",          texANIME_BUG_PATCH,
-  NULL, nullptr
-};
 
 // 00334f30
 const std::array<SPI_TAG_PARAM, 48> effm_tag =
