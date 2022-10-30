4d003b1298206c98c2d65e5b67630c8b7cb3379e
raSTARgfx <rastargfx@protonmail.com>
2022-10-29 20:36:42 -0400

    move effm_tag to CEffectManager

--- dc2/Tags.h
+++ dc2/Tags.h
@@ -2,7 +2,6 @@
 #include "common/types.h"
 #include "SpiStack.h"
 
-extern const std::array<SPI_TAG_PARAM, 48> effm_tag;
 extern const std::array<SPI_TAG_PARAM, 5>  skin_tag;
 extern const std::array<SPI_TAG_PARAM, 12> tree_map_tag;
 extern const std::array<SPI_TAG_PARAM, 25> gamedata_tag;
