d9be2129937169f42124ec22484953ead9cdedf7
raSTARgfx <rastargfx@protonmail.com>
2022-10-31 16:02:27 -0400

    add logging and todo to CGamePad

--- dc2/CGamePad.cc
+++ dc2/CGamePad.cc
@@ -1,10 +1,16 @@
+#include <cmath>
+#include "common/debug.h"
+#include "common/log.h"
+
 #include "CGamePad.h"
 
-#include <cmath>
+set_log_channel("CGamePad");
 
 // 0014aef0
 static sint AxisCalibration(sint axis)
 {
+  log_trace("AxisCalibration({})", axis);
+
   // dead zone
   if(std::abs(axis - 128) < 50)
     return 0;
@@ -17,40 +23,64 @@ static sint AxisCalibration(sint axis)
 
 sint CGamePad::GetRX()
 {
+  log_trace("GetRX()");
+
+  todo;
+
   return 0; // TODO
 }
 
 sint CGamePad::GetRY()
 {
+  log_trace("GetRY()");
+
+  todo;
+
   return 0; // TODO
 }
 
 sint CGamePad::GetLX()
 {
+  log_trace("GetLX()");
+
+  todo;
+
   return 0; // TODO
 }
 
 sint CGamePad::GetLY()
 {
+  log_trace("GetRY()");
+
+  todo;
+
   return 0; // TODO
 }
 
 f32 CGamePad::GetRXf()
 {
+  log_trace("GetRXf()");
+
   return static_cast<f32>(GetRX()) / 128.0f;
 }
 
 f32 CGamePad::GetRYf()
 {
+  log_trace("GetRYf()");
+
   return static_cast<f32>(GetRY()) / 128.0f;
 }
 
 f32 CGamePad::GetLXf()
 {
+  log_trace("GetLXf()");
+
   return static_cast<f32>(GetLX()) / 128.0f;
 }
 
 f32 CGamePad::GetLYf()
 {
+  log_trace("GetLYf()");
+
   return static_cast<f32>(GetLY()) / 128.0f;
 }
