b45048c5c4bb83387cd1235c08a4e07b9963ecb5
raSTARgfx <rastargfx@protonmail.com>
2022-10-31 12:11:53 -0400

    add some logging and functions in CDynamicAnime

--- dc2/CDynamicAnime.cc
+++ dc2/CDynamicAnime.cc
@@ -1,128 +1,265 @@
+#include "common/debug.h"
+#include "common/log.h"
+
 #include "CDynamicAnime.h"
 #include "CScriptInterpreter.h"
 
-static bool dynFRAME_START(SPI_STACK* stack, int)
+set_log_channel("CDynamicAnime");
+
+CDynamicAnime* dynNowDA{ nullptr };
+mgCMemory* dynStack{ nullptr };
+
+// 0017b160
+static bool dynFRAME_START(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFRAME(SPI_STACK* stack, int)
+// 0017b190
+static bool dynFRAME(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFRAME_END(SPI_STACK* stack, int)
+// 0017b210
+static bool dynFRAME_END(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  // this function actually just returns true
   return true;
 }
 
-static bool dynVERTEX_START(SPI_STACK* stack, int)
+// 0017b220
+static bool dynVERTEX_START(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  const int var = spiGetStackInt(stack);
+  dynNowDA->NewVertexTable(var, dynStack);
+
   return true;
 }
 
-static bool dynVERTEX_L(SPI_STACK* stack, int)
+// 0017b300
+static bool dynVERTEX_L(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  const int frame_id = spiGetStackInt(stack);
+
+  vec3 vec{ };
+  spiGetStackVector(vec, ++stack);
+
+  todo;
+
   return true;
 }
 
-static bool dynVERTEX_END(SPI_STACK* stack, int)
+// 0017b390
+static bool dynVERTEX_END(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFIX_VERTEX_START(SPI_STACK* stack, int)
+// 0017b410
+static bool dynFIX_VERTEX_START(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFIX_VERTEX(SPI_STACK* stack, int)
+// 0017b530
+static bool dynFIX_VERTEX(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFIX_VERTEX_C(SPI_STACK* stack, int)
+// 0017b5c0
+static bool dynFIX_VERTEX_C(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFIX_VERTEX_S(SPI_STACK* stack, int)
+// 0017b650
+static bool dynFIX_VERTEX_S(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFIX_VERTEX_END(SPI_STACK* stack, int)
+// 0017b6e0
+static bool dynFIX_VERTEX_END(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFRAME_POSE(SPI_STACK* stack, int)
+// 0017b8b0
+static bool dynFRAME_POSE(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynFRAME_POSE_L(SPI_STACK* stack, int)
+// 0017b8b0
+static bool dynFRAME_POSE_L(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynDRAW_FRAME(SPI_STACK* stack, int)
+// 0017b970
+static bool dynDRAW_FRAME(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynBIND_VERTEX_START(SPI_STACK* stack, int)
+// 0017b9f0
+static bool dynBIND_VERTEX_START(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynBIND_VERTEX(SPI_STACK* stack, int)
+// 0017ba20
+static bool dynBIND_VERTEX(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynBIND_VERTEX_END(SPI_STACK* stack, int)
+// 0017bb80
+static bool dynBIND_VERTEX_END(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  // this function actually just returns true
   return true;
 }
 
-static bool dynBOUNDING_BOX_START(SPI_STACK* stack, int)
+// 0017bb90
+static bool dynBOUNDING_BOX_START(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  const int var = spiGetStackInt(stack);
+  dynNowDA->NewBoundingBoxTable(var, dynStack);
+
   return true;
 }
 
-static bool dynBOUNDING_BOX(SPI_STACK* stack, int)
+// 0017bbc0
+static bool dynBOUNDING_BOX(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynBOUNDING_BOX_END(SPI_STACK* stack, int)
+// 0017bc70
+static bool dynBOUNDING_BOX_END(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  // this function actually just returns true
   return true;
 }
 
-static bool dynCOLLISION_START(SPI_STACK* stack, int)
+// 0017bc80
+static bool dynCOLLISION_START(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  const int var = spiGetStackInt(stack);
+  dynNowDA->NewCollisionTable(var, dynStack);
+
   return true;
 }
 
-static bool dynCOLLISION(SPI_STACK* stack, int)
+// 0017bcb0
+static bool dynCOLLISION(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynCOLLISION_END(SPI_STACK* stack, int)
+// 0017be60
+static bool dynCOLLISION_END(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  // this function actuall just returns true
   return true;
 }
 
-static bool dynGRAVITY(SPI_STACK* stack, int)
+// 0017be70
+static bool dynGRAVITY(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
-static bool dynWIND(SPI_STACK* stack, int)
+// 0017bed0
+static bool dynWIND(SPI_STACK* stack, int stack_count)
 {
+  trace_script_call(stack, stack_count);
+
+  todo;
+
   return true;
 }
 
@@ -156,5 +293,28 @@ const std::array<SPI_TAG_PARAM, 27> dynmc_tag =
 
 void CDynamicAnime::Load(const char* script, int script_size, mgCFrame* frame, mgCMemory* stack)
 {
-  // TODO
+  log_trace("Load({}, {}, {}, {})", fmt::ptr(script), script_size, fmt::ptr(frame), fmt::ptr(stack));
+
+  todo;
+}
+
+void CDynamicAnime::NewVertexTable(int unk, mgCMemory* stack)
+{
+  log_trace("NewVertexTable({}, {})", unk, fmt::ptr(stack));
+
+  todo;
+}
+
+void CDynamicAnime::NewBoundingBoxTable(int unk, mgCMemory* stack)
+{
+  log_trace("NewBoundingBoxTable({}, {})", unk, fmt::ptr(stack));
+
+  todo;
+}
+
+void CDynamicAnime::NewCollisionTable(int unk, mgCMemory* stack)
+{
+  log_trace("NewCollisionTable({}, {})", unk, fmt::ptr(stack));
+
+  todo;
 }
