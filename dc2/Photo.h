41d01068dd08da2a3e6151fd289d0a151577459d
Dakota Smith <dakotachasesmith@gmail.com>
2022-10-30 15:51:55 -1000

    photo: Move static variables into cc file

--- dc2/Photo.h
+++ dc2/Photo.h
@@ -1,11 +1,9 @@
 ﻿#pragma once
 #include <array>
 #include "common/types.h"
-#include "CFont.h"
 #include "CPadControl.h"
 #include "CInventUserData.h"
 #include "mgCMemory.h"
-#include "mgCTexture.h"
 
 namespace Photo
 {
@@ -67,69 +65,3 @@ void SetTookPhotoData(USER_PICTURE_INFO& picture_info);
 
 // 0030F6A0
 void DrawTakePhotoSystem(s32 i1, CInventUserData& invest_user_data);
-
-// 0035E6A0
-constexpr static std::array<std::array<const char*, 4>, 2> msg_text = {
-	// Japanese (4)
-	"(A):写真を確認",
-	"(#):写真をとる",
-	"ユリスの写真家レベルが上がった",
-	"(R):ズーム",
-
-	// English (4)
-	"(A):Confirm Picture",
-	"(#):Take Picture",
-	"Max's photography level increased!",
-	"(R):zoom (O):Back",
-};
-// 0035E6C0
-constexpr static vec4 stru_35E6C0 = { 0.0f, 0.0f, 0.0f, 1.0f };
-// 0035E6D0
-constexpr static u64 qword_35E6D0 = 0x100000000;
-// 0035E6D8
-constexpr static u32 dword_35E6D8 = 2;
-// 0035E6E0
-constexpr static u64 qword_35E6E0 = 0x2FFFFFFFF;
-// 0035E6E8
-constexpr static float flt_35E6E8 = 0.0f;
-// 0035E6F0
-constexpr static u64 qword_35E6F0 = 0xFFFFFFFF;
-// 0035E6F8
-constexpr static u32 dword_35E6F8 = 2;
-// 0035E700
-constexpr static vec4 stru_35E700 = { -1.0f, 3.0f, -3.0f, 1.0f };
-// 0035E710
-constexpr static vec4 stru_35E710 = { 2.0f, -5.0f, 4.0f, -1.0f };
-// 0035E720
-constexpr static vec4 stru_35E720 = { -1.0f, 0.0f, 1.0f, 0.0f };
-// 0035E730
-constexpr static vec4 stru_35E730 = { 0.0f, 2.0f, 0.0f, 0.0f };
-// 0035E740
-constexpr static vec4 stru_35E740 = { 0.0f, 0.0f, 0.0f, 1.0f };
-
-// 00376B30
-constexpr static const char null_txt[] = "";
-
-// 00378710
-static u32 dword_378710;
-// 00378714
-static float flt_378714;
-// 00378718
-static int dword_378718;
-// 0037871C
-static mgCTexture* dword_37871C;
-// 00378720
-static s32 dword_378720;
-// 00378724
-static s32 dword_378724;
-// 00378728
-static bool dword_378728;
-// 0037872C
-static int dword_37872C;
-// 00378730
-static int dword_378730;
-
-// 01F5DDF0
-static CFont stru_1F5DDF0;
-// 01F5DEA0
-static char byte_1F5DEA0[0x80];
