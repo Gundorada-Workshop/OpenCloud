98c3f0c6f17ec5a57b3f98d5ec9564317ac495a3
Dakota Smith <dakotachasesmith@gmail.com>
2022-10-31 21:10:59 -1000

    CVillager: Stub CVillager classes

---
+++ dc2/CVillager.h
@@ -0,0 +1,36 @@
+#pragma once
+
+class CVillagerPlace
+{
+public:
+
+  struct ProgressInfo
+  {
+  public:
+
+  };
+};
+
+class CVillagerPlaceInfo
+{
+public:
+
+};
+
+class CVillagerData
+{
+public:
+
+};
+
+class CVillagerMngr
+{
+public:
+
+};
+
+class CVillagerInfo
+{
+public:
+
+};
