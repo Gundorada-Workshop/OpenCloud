c2ed1eb1a731b9aad4510ee5309573a42a7740d6
Dakota Smith <dakotachasesmith@gmail.com>
2022-11-04 16:35:31 -1000

    sysmes: Implement sinit

--- dc2/sinit.cc
+++ dc2/sinit.cc
@@ -6,6 +6,7 @@
 #include "mg_lib.h"
 #include "mg_tanime.h"
 #include "snd_mngr.h"
+#include "sysmes.h"
 #include "pbuggy.h"
 #include "photo.h"
 #include "title.h"
@@ -21,6 +22,7 @@ namespace StaticInitializers
 		mg_lib::SInit();
 		mg_tanime::SInit();
 		snd_mngr::SInit();
+		sysmes::SInit();
 		PBuggy::SInit();
 		Photo::SInit();
 		Title::SInit();
--- dc2/sysmes.h
+++ dc2/sysmes.h
@@ -1,3 +1,9 @@
 #pragma once
 
 // ~ 00196130 - 00196BE0
+
+namespace sysmes
+{
+  // 003739D0
+  void SInit();
+}
