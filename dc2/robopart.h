2aba62769cf126330291b3ec2dc23d58aa9ab3a1
Dakota Smith <dakotachasesmith@gmail.com>
2022-11-05 15:25:22 -1000

    robopart: Define ROBO_INFO_DATA struct

--- dc2/robopart.h
+++ dc2/robopart.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <array>
+#include "common/debug.h"
 
 // ~ 001B5FD0 - 001BAA30
 
@@ -11,3 +13,27 @@ class CLaserGunMan {};
 class CPullItem {};
 class CPullItemManager {};
 class CRoboVoiceSystem {};
+
+struct ROBO_INFO_DATA
+{
+  // 0
+  std::array<char, 0x10>* m_unk_field_0;
+  // 4
+  std::array<char, 0x10>* m_unk_field_4;
+  // 8
+  std::array<char, 0x10>* m_unk_field_8;
+  // C
+  std::array<char, 0x40>* m_unk_field_C;
+  // 10
+  std::array<char, 0x10>* m_unk_field_10;
+  // 14
+  char* m_unk_field_14;
+  // 18
+  _UNKNOWNPOINTER m_unk_field_18;
+  // 1C
+  // FIXME: enum type?
+  _DWORD m_robo_info_type1;
+  // 20
+  // FIXME: enum type?
+  _DWORD m_robo_info_type2;
+};
