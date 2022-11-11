158fc2b329c516b192150f0e73a140c33ac5ffe0
Dakota Smith <dakotachasesmith@gmail.com>
2022-11-10 23:23:55 -1000

    convviewlp: static init

--- dc2/convviewlp.cc
+++ dc2/convviewlp.cc
@@ -1,1 +1,10 @@
+#include "common/log.h"
 #include "convviewlp.h"
+#include "mg_lib.h"
+
+set_log_channel("convviewlp");
+
+// 01F64AA0
+static mgCMemory DataBuffer{};
+// 01F64AD0
+static mgCMemory Stack_ReadBuff{};
