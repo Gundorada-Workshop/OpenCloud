aa5c4d18abc1f267c09058e099e59d4e47e39e4d
Dakota Smith <dakotachasesmith@gmail.com>
2022-11-10 20:48:24 -1000

    menuop: static init

--- dc2/menuop.cc
+++ dc2/menuop.cc
@@ -1,1 +1,12 @@
+#include "common/log.h"
 #include "menuop.h"
+#include "mg_lib.h"
+
+set_log_channel("menuop");
+
+// 01F0D200
+static mgCMemory StaticMenuLocalStack{};
+// 01F0D230
+static mgCMemory StaticMenuLocalStack2{};
+// 01F0D260
+static mgCMemory SaveMenuStack{};
