ec41f3871532813799a460441b19ff71294a18a1
Dakota Smith <dakotachasesmith@gmail.com>
2022-11-10 23:19:00 -1000

    vlgr_info: Static init

--- dc2/vlgr_info.cc
+++ dc2/vlgr_info.cc
@@ -1,1 +1,9 @@
+#include <array>
+#include "common/log.h"
+#include "villager.h"
 #include "vlgr_info.h"
+
+set_log_channel("vlgr_info");
+
+// 01F62BD0
+static std::array<CVillagerPlace, 0x200> VlgrPlace{};
