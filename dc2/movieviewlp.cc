927604c0d072d0d70ab7024f8c5f47bed6eade69
Dakota Smith <dakotachasesmith@gmail.com>
2022-11-10 20:50:31 -1000

    movieviewlp: static init

--- dc2/movieviewlp.cc
+++ dc2/movieviewlp.cc
@@ -1,1 +1,10 @@
+#include "common/log.h"
+#include "mg_lib.h"
 #include "movieviewlp.h"
+
+set_log_channel("movieviewlp");
+
+// 01F0D300
+static mgCMemory DataBuffer{};
+// 01F0D330
+static mgCMemory Stack_ReadBuff{};
