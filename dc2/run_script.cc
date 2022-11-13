80d01b687e458317c2da2bab74174db54849ef6b
Dakota Smith <dakotachasesmith@gmail.com>
2022-11-13 01:32:31 -1000

    run_script: Implement _SIN

--- dc2/run_script.cc
+++ dc2/run_script.cc
@@ -650,9 +650,24 @@ void CRunScript::exe(vmcode_t* code)
         m_vmcode += 1;
         return;
       case 29:
+      {
         // 00188398
-        todo;
+        // _SIN
+        auto lhs = pop();
+        if (lhs.m_data_type == Type::Int)
+        {
+          push_float(sinf(static_cast<f32>(lhs.m_data.i)));
+        }
+        else if (lhs.m_data_type == Type::Float)
+        {
+          push_float(sinf(lhs.m_data.f));
+        }
+        else
+        {
+          panicf("RUNTIME ERROR at _SIN: {}: operand is not a number", m_current_funcdata->m_function_name);
+        }
         break;
+      }
       case 30:
         // 00188438
         todo;
