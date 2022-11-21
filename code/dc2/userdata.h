f4d4325aad48175e9d489fb73554289ba5b2ff61
Dakota Smith <dakotachasesmith@gmail.com>
2022-11-21 11:14:45 -1000

    userdata: Implement ItemCmdMsgSet

--- code/dc2/userdata.h
+++ code/dc2/userdata.h
@@ -5,10 +5,46 @@
 #include "dc2/gamedata.h"
 #include "dc2/inventmn.h"
 
-// ~ 00196C20 - 001A2080
+// ~ 00196130 - 001A2080
 
 // TODO THIS FILE
 struct MOS_CHANGE_PARAM {};
+
+enum class EItemCmd
+{
+  _0 = 0,
+  _1 = 1,
+  _2 = 2,
+  _3 = 3,
+  _4 = 4,
+  _5 = 5,
+  _6 = 6,
+  _7 = 7,
+  _8 = 8,
+  _9 = 9,
+  _10 = 10,
+  _11 = 11,
+  _12 = 12,
+  _13 = 13,
+  _14 = 14,
+  _15 = 15,
+  _16 = 16,
+  _17 = 17,
+  _18 = 18,
+  _19 = 19,
+  _20 = 20,
+  _21 = 21,
+  _22 = 22,
+  _23 = 23,
+  _24 = 24,
+  _25 = 25,
+  _26 = 26,
+  _27 = 27,
+  _28 = 28,
+  _29 = 29,
+  _30 = 30,
+  _31 = 31,
+};
 
 enum class ECharacterID
 {
@@ -551,6 +587,9 @@ public:
 
   // Size 0x90
 };
+
+// 00196130
+usize ItemCmdMsgSet(EItemCmd cmd, s32* dest);
 
 // 00196520
 bool CheckItemEquip(ECharacterID chara_id, ECommonItemData item_id);
