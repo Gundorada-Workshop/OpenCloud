1e3a6fb67765c4be89099e65210fac58f01f955c
raSTARgfx <rastargfx@protonmail.com>
2022-11-21 20:28:40 -0500

    cleanup dng_event

--- code/dc2/dng_event.h
+++ code/dc2/dng_event.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <array>
 
-#include <glm/glm.hpp>
-
 #include "dc2/character.h"
 #include "dc2/dng_main.h"
 #include "dc2/event_func.h"
@@ -10,11 +8,25 @@
 // ~ 0028AFC0 - 002912F0
 
 // TODO THIS FILE
-class CStartupEpisodeTitle {};
-struct MessageTaskManager {};
-class CRedMarkModel {};
-class CTreasureBox {};
-class CTreasureBoxManager {};
+class CStartupEpisodeTitle
+{
+};
+
+struct MessageTaskManager
+{
+};
+
+class CRedMarkModel
+{
+};
+
+class CTreasureBox
+{
+};
+
+class CTreasureBoxManager
+{
+};
 
 class CGeoStone : public CCharacter2
 {
@@ -23,9 +35,9 @@ public:
   virtual void Initialize();
 
   // 0028BCA0
-  bool CheckEvent(glm::vec4& position);
+  bool CheckEvent(vec4& position);
   // 0028BA80
-  void GeoDraw(glm::vec4& position);
+  void GeoDraw(vec4& position);
   // 0028BB70
   void DrawMiniMapSymbol(CMiniMapSymbol* mini_map_symbol);
   // 0028BBC0
@@ -37,7 +49,7 @@ public:
   // 660
   bool m_flag{false};
   // 664
-  float m_height_offset_sine{ 0.0f };
+  f32 m_height_offset_sine{ 0.0f };
   // 668
   bool m_unk_field_668{ false };
 
@@ -48,24 +60,24 @@ class CRandomCircle
 {
 public:
   // 0028BD50
-  void Draw(glm::vec4* v);
+  void Draw(vec4* v);
   // 0028BE40
   void Step();
   // 0028BE60
   void DrawSymbol(CMiniMapSymbol* mini_map_symbol);
   // 0028BEF0
-  bool CheckArea(glm::vec4* v, float f);
+  bool CheckArea(vec4* v, f32 f);
   // 0028BFA0
-  bool GetPosition(glm::vec4* dest, ssize i);
+  bool GetPosition(vec4* dest, ssize i);
   // 0028C020
-  ssize CheckEvent(glm::vec4* v);
+  ssize CheckEvent(vec4* v);
   // 0028C0D0
-  ssize SetCircle(glm::vec4* v);
+  ssize SetCircle(vec4* v);
   // 0028C160
   void Clear();
 
   // 0
-  std::array<glm::vec4, 3> m_unk_field_0{};
+  std::array<vec4, 3> m_unk_field_0{};
   // 30
   std::array<bool, 3> m_unk_field_30{ false };
   // 3C
