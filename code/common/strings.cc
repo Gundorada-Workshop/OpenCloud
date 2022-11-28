ca05c39b98b242cfe5b9d9229aeb36f7c83fc3cc
raSTARgfx <rastargfx@protonmail.com>
2022-11-28 15:23:49 -0500

    fix bug in conversion functions
    make the conversion functions return an empty string when an empty string is passed rather than a failure condition

--- code/common/strings.cc
+++ code/common/strings.cc
@@ -9,7 +9,7 @@ namespace common::strings
   std::optional<std::string> wstring_to_utf8(std::wstring_view wide)
   {
     if (wide.empty())
-      return { };
+      return "";
 
     auto size = WideCharToMultiByte(CP_UTF8, 0, wide.data(),
       static_cast<int>(wide.size()), nullptr, 0, 0, nullptr);
@@ -32,7 +32,7 @@ namespace common::strings
   std::optional<std::wstring> utf8_to_wstring(std::string_view utf8)
   {
     if (utf8.empty())
-      return { };
+      return L"";
 
     auto size = MultiByteToWideChar(CP_UTF8, 0, utf8.data(), static_cast<int>(utf8.size()), nullptr, 0);
 
@@ -56,7 +56,7 @@ namespace common::strings
     static constexpr UINT CP_SJIS = 932;
 
     if (sjis.empty())
-      return { };
+      return "";
 
     auto size = MultiByteToWideChar(CP_SJIS, 0, sjis.data(), static_cast<int>(sjis.size()), nullptr, 0);
     if (size < 1)
