35cf294f6359ad945476c9543b73e6ac266dae5c
raSTARgfx <rastargfx@protonmail.com>
2022-11-29 10:00:35 -0500

    cleanup collision.h

--- code/dc2/collision.h
+++ code/dc2/collision.h
@@ -3,9 +3,26 @@
 // ~ 00147450 - 00148650
 
 // THIS FILE TODO
-class CCollision {};
-class CCollisionMDT {};
-class CColFrame {};
-class CColPrim {};
-class CColPrimMan {};
-class CCPoly {};
+class CCollision
+{
+};
+
+class CCollisionMDT
+{
+};
+
+class CColFrame
+{
+};
+
+class CColPrim
+{
+};
+
+class CColPrimMan
+{
+};
+
+class CCPoly
+{
+};
