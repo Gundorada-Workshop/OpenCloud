af5f493ee5c67dcc8261a84f81d80694fdd62c49
raSTARgfx <rastargfx@protonmail.com>
2022-12-02 15:17:52 -0500

    fill out some of SCElogoFade

--- code/dc2/mainloop.cc
+++ code/dc2/mainloop.cc
@@ -2,6 +2,8 @@
 
 #include "common/debug.h"
 #include "common/log.h"
+#include "common/data_stream.h"
+#include "common/file_helpers.h"
 
 #include "host/host_interface_dwm.h"
 
@@ -17,6 +19,8 @@
 #include "dc2/userdata.h"
 
 set_log_channel("mainloop")
+
+using namespace common;
 
 // 00376FB4
 u32 s_system_snd_id{ 0 };
@@ -110,6 +114,93 @@ bool GetPlayTimeCountFlag()
   return PlayTimeCountFlag;
 }
 
+// 0030a290
+// I don't know where this belongs but it's only used by MainLoop
+static void SCElogoFade(bool fade_out, mgCMemory* stack_mem)
+{
+  // we don't need vif packets
+  // auto vifpk_a = static_cast<u128*>(stack_mem->stAlloc64(10000));
+  // auto vifpk_b = static_cast<u128*>(stack_mem->stAlloc64(10000));
+  // mgInitVifPacket(vifpk_a, vifpk_b);
+  //
+  // mgCMemory mem_a{ };
+  // auto a = stack_mem->stAlloc64(10000);
+  // mem_a.stSetBuffer(a, 10000);
+  //
+  // mgCMemory mem_b{ };
+  // auto b = stack_mem->stAlloc64(10000);
+  // mem_b.stSetBuffer(b, 10000);
+  //
+  // mgSetPacketBuffer(&mem_a, &mem_b);
+  //
+  // mgCMemory mem_d{ };
+  // auto c = stack_mem->stAlloc64(10000);
+  // mem_c.stSetBuffer(c, 10000);
+  //
+  // mgCMemory mem_b{ };
+  // auto c = stack_mem->stAlloc64(10000);
+  // mem_c.stSetBuffer(c, 10000);
+  //
+  // mgSetDataBuffer(&mem_c, &mem_d, 1);
+  //
+  // mgTexManager.SetTableBuffer(10, 10, stack_mem);
+
+  todo;
+  // auto vram_top = mgGetTopVRAMAddress();
+  // mgTexManager.Initialize(vram_top,-1);
+  //
+  // stack_mem->Align64();
+  //
+  // u8* image_buffer = static_cast<u8*>(stack_mem->m_stack_start) + stack_mem->m_stack_current_allocated * 16;
+  // 
+  // int size;
+  // auto err = LoadFile2("title/title1.img", image_buffer, &size, 0);
+
+  auto [buff, size] = g_host_interface->open_game_file_cached("title/title1.img");
+  if (!buff || !size)
+    panicf("Failed to open title1.img");
+
+  mgTexManager.EnterIMGFile(buff.get(), 0, nullptr, nullptr);
+
+  uint factor = 0;
+  for (uint i = 0; i < 24; ++i)
+  {
+    // using enum mgCDrawPrim::Primitive;
+
+    mgBeginFrame(nullptr);
+
+    // mgTexManager.ReloadTexture(0, nullptr);
+    // auto moji_texture = mgTexManager.GetTexture("moji", -1);
+    //
+    // mgCDrawPrim dp{};
+    // dp.AlphaBlendEnable(true);
+    // dp.TextureMapEnable(true);
+    //
+    // dp.Begin(Sprite);
+    //
+    // auto alpha = std::min(factor / 20u, 128u);
+    //
+    // if (fade_out)
+    //   alpha = 128u - alpha;
+    //
+    // dp.Color(128, 128, 128, alpha);
+    //
+    // dp.Texture(moji_texture);
+    //
+    // dp.TextureCrd(0, 324);
+    // dp.Vertex(0, 176, 0);
+    //
+    // dp.TextureCrd(512, 384);
+    // dp.Vertex(512, 236, 0);
+    //
+    // dp.End();
+
+    mgEndFrame(nullptr);
+
+    factor += 128;
+  }
+}
+
 // 00190CB0
 void MainLoop()
 {
@@ -134,6 +225,7 @@ void MainLoop()
 
   todo;
 
+  SCElogoFade(false, nullptr);
   g_host_interface->set_frame_divider(1);
 
   while (!g_host_interface->message_pump_quit_requested())
