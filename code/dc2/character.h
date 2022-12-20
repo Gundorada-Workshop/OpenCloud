5d8ad55c5f2d93d004d85f78634ba569465487ba
Dakota Smith <dakotachasesmith@gmail.com>
2022-12-19 17:05:24 -1000

    character: Stub CActionChara member fns

--- code/dc2/character.h
+++ code/dc2/character.h
@@ -9,6 +9,9 @@
 
 // ~ 0016A140 - 0017C240
 
+// forward-declaration
+class CScene;
+
 // TODO THIS FILE
 class CDACollision {};
 
@@ -53,6 +56,12 @@ struct CHRINFO_KEY_SET
 struct SEQ_HEADER
 {
 
+};
+
+// This might be for another file but putting this here for now
+struct SOUND_INFO
+{
+  // SIZE 0x28
 };
 
 class CCharacter2 : public CObjectFrame
@@ -480,10 +489,10 @@ public:
   f32 m_unk_field_50C{ 0.0f };
 
   // 510
-  std::array<std::vector<std::shared_ptr<CHRINFO_KEY_SET>>, 8> m_CHRINFO_KEY_SETs{};
+  std::array<std::vector<std::shared_ptr<CHRINFO_KEY_SET>>, 8> m_motion_keys{};
 
   // 530
-  // std::array<usize, 8> m_n_CHRINFO_KEY_SETs{};
+  // std::array<usize, 8> m_n_motion_keys{};
 
   // 550
   std::array<std::shared_ptr<SEQ_HEADER>, 8> m_seq_headers{};
@@ -492,34 +501,7 @@ public:
   std::array<CSwordAfterEffect*, 3> m_sword_after_effects;
 
   // 57C
-  unk32 m_unk_field_57C{ 0 };
-
-  // 580
-  unk32 m_unk_field_580{ 0 };
-
-  // 584
-  unk32 m_unk_field_584{ 0 };
-
-  // 588
-  unk32 m_unk_field_588{ 0 };
-
-  // 58C
-  unk32 m_unk_field_58C{ 0 };
-
-  // 590
-  unk32 m_unk_field_590{ 0 };
-
-  // 594
-  unk32 m_unk_field_594{ 0 };
-
-  // 598
-  unk32 m_unk_field_598{ 0 };
-
-  // 59C
-  unk32 m_unk_field_59C{ 0 };
-
-  // 5A0
-  unk32 m_unk_field_5A0{ 0 };
+  SOUND_INFO m_sound_info{};
 
   // 5A4
   unk32 m_unk_field_5A4{ 0 };
@@ -621,15 +603,215 @@ enum class EMoveType : s32
 
 };
 
+// this might be from another file but putting them here for now
+struct SW_EFFECT
+{
+  // SIZE 0x20
+};
+
 constexpr f32 DEFAULT_CHARACTER_MOVE_SPEED = 3.0f;
 
 class CActionChara : public CCharacter2
 {
 public:
   
+  // 0016B850
+  virtual unkptr Draw() override;
+
+  // 0016B940
+  virtual unkptr DrawDirect() override;
+
+  // 0016AB60
+  virtual f32 GetCameraDist() override;
+
+  // 0016AB00
+  virtual void SetFarDist(f32 far_dist) override;
+
+  // 0016AB30
+  virtual void SetNearDist(f32 near_dist) override;
+
+  //001739A0
+  virtual void SetMotion(std::string key_name, sint i1) override;
+
+  // 0016B800
+  virtual void ResetMotion() override;
+
+  // 0016AAD0
+  virtual void SetFadeFlag(bool flag) override;
+
+  // 0016BA70
+  virtual unkptr DrawShadowDirect() override;
+
+  // 00171E80
+  virtual void Step() override;
+
+  // 00172090
+  virtual void ShadowStep() override;
+
+  // 0016BAC0
+  virtual void DrawEffect() override;
+
+  // 0016ABA0
+  virtual void Show(sint i1, sint i2);
+
+  // 0016ABE0
+  virtual void GetShow(std::string chara_name);
+
+  // 0016B780
+  virtual void SetMotion(std::string key_name, sint i1, sint i2);
+
+  // 0016B5B0
+  virtual ECharacterMotionStatus GetMotionStatus(std::string chara_name);
+
+  // 0016BB00
+  virtual void StepEffect();
+
+  // if you call either of these instead of creating/using a constructor/copy constructor I'll keelhaul you
+  // 001720E0
+  virtual void Initialize(mgCMemory* memory);
+
+  // 00172380
+  virtual void Copy(CActionChara& other, mgCMemory* memory);
+
+  // 0016A140
+  void ResetAccele();
+
+  // 0016A160
+  void ResetAction();
+
   // 0016A220
   void ResetScript();
 
+  // 0016A4C0
+  void CheckRunEvent();
+
+  // 0016A4E0
+  void SetMaskFlag(u32 mask, bool active);
+
+  // 0016A510
+  unkptr EntryObject(std::string frame_name, ssize i);
+
+  // 0016A5C0
+  void CalcCollision();
+
+  // 0016A620
+  unkptr EntryBodyCol(ssize i, f32 f);
+
+  // 0016A6B0
+  unkptr EntryDamage2(std::string s1, std::string s2, std::string s3, f32 f1, std::string s4, f32 f2, f32 f3, std::string s5);
+  
+  // 0016A850
+  unkptr EntryDamage2(mgCFrame* frame1, mgCFrame* frame2, std::string s3, f32 f1, std::string s4, f32 f2, f32 f3, std::string s5);
+
+  // 0016A9B0
+  void AllDeleteDamage();
+
+  // 0016AA30
+  SW_EFFECT* GetSwEffectPtr();
+
+  // 0016AA70
+  void SetSoundInfoCopy();
+
+  // 0016AC60
+  bool CheckKeri(std::string frame_name, bool b);
+
+  // 0016AD30
+  bool CheckEnemyCatch(std::string frame_name);
+
+  // 0016AF40
+  void ThrowItemObject();
+
+  // 0016B020
+  void UsedItemAction();
+
+  // 0016B1C0
+  void EntryThrowItem();
+
+  // 0016B3C0
+  void RemoveThrowItem();
+
+  // 0016B420
+  f32 GetNowFrameWait(std::string chara_name);
+
+  // 0016B4A0
+  f32 GetNowFrame(std::string chara_name);
+
+  // 0016B520
+  void CheckMotionEnd(std::string chara_name);
+
+  // 0016B630
+  f32 GetWaitToFrame(std::string chara_name);
+
+  // 0016BC90
+  CActionChara* SearchChara(std::string chara_name);
+
+  // 0016BCF0
+  mgCFrame* SearchObject(std::string frame_name);
+
+  // 0016BD60
+  void ResetParent();
+
+  // 0016BD90
+  bool SetRef(CActionChara* parent_chara, std::string frame_name);
+
+  // 0016BE50
+  f32 GetTargetDist(CScene* scene);
+
+  // 0016BEE0
+  void CollisionCheck(const vec3& v1, const vec3& v2, const vec3& v3);
+
+  // 0016C7A0
+  // "RockOn"
+  void LockOn();
+
+  // 0016C990
+  void HumanMoveIF();
+
+  // 0016D560
+  void HumanShrowMoveIF();
+
+  // 0016D8E0
+  void HumanTameMoveIF();
+
+  // 0016DAA0
+  void HumanGunMoveIF();
+
+  // 0016DD80
+  void RoboWalkMoveIF(sint i);
+
+  // 0016E2A0
+  void RoboTankMoveIF(sint i);
+
+  // 0016E9F0
+  void RoboBikeMoveIF(sint i);
+
+  // 0016F330
+  void RoboAirMoveIF(sint i1, sint i2);
+
+  // 0016FA30
+  void MonsterMoveIF();
+
+  // 00170730
+  bool CheckDamage();
+
+  // 001710C0
+  void LoadActionFile(char* c, int i, mgCMemory* memory);
+
+  // 00171160
+  void InitScript();
+
+  // 00171210
+  void RunScript(CScene* scene, RUN_SCRIPT_ENV* script_env);
+
+  // 001719C0
+  // "CheckReleaseTimming"
+  s16 CheckReleaseTiming(sint i);
+
+  // 001719F0
+  void StepParam();
+
+  // 678
+  CActionChara* m_chara_parent{};
   // 690
   vec3 m_front_vec{ 0.0f, 0.0f, 0.0f };
   // 6A4
@@ -640,4 +822,6 @@ public:
   s16 m_prog{ 0 };
   // 794
   f32 m_move_speed{ DEFAULT_CHARACTER_MOVE_SPEED };
+  // 7E4
+  std::array<SW_EFFECT, 9> m_unk_field_7E4{};
 };
