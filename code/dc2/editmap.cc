e2eec6f19ea3cc464bbb82a6739740a1345ccf10
Dakota Smith <dakotachasesmith@gmail.com>
2022-12-31 14:30:46 -1000

    editmap: Implement CEditInfoMngr class

---
+++ code/dc2/editmap.cc
@@ -0,0 +1,152 @@
+#include "common/log.h"
+
+#include "dc2/editmap.h"
+#include "dc2/script_interpreter.h"
+
+set_log_channel("editmap");
+
+// 003543C0
+const static SPI_TAG_PARAM emap_tag[] = {
+  // TODO
+  nullptr, nullptr
+};
+
+// 00377F44
+static CEditInfoMngr* emapInfo{};
+// 00377F4C
+static sint emapIdx{};
+// 00377F50
+static sint emapMatID{};
+// 00377F54
+static CEditInfoMngr* emapNowInfo{};
+// 00377F58
+static sint emapRectType{};
+// 00377F5C
+static rect* emapRect{};
+// 00377F60
+static sint emapRectNum{};
+// 00377F64
+static sint emapRectIdx{};
+// 00377F68
+static sint emapFixNum{};
+// 00377F6C
+static sint emapFixIdx{};
+
+// 001B5630
+EEPartsType CEditPartsInfo::GetPartsType() const
+{
+  log_trace("CEditPartsInfo::{}()", __func__);
+
+  todo;
+  return EEPartsType(0);
+}
+
+// 002A4DE0
+void CEditInfoMngr::SetePartsInfoTable(usize capacity)
+{
+  log_trace("CEditInfoMngr::{}({})", __func__, capacity);
+
+  m_eparts_info.clear();
+  m_eparts_info.reserve(capacity);
+
+  for (usize i = 0; i < capacity; ++i)
+  {
+    m_eparts_info.emplace_back();
+  }
+}
+
+// 002A4DF0
+void CEditInfoMngr::SeteFixPartsTable(usize capacity)
+{
+  log_trace("CEditInfoMngr::{}({})", __func__, capacity);
+
+  m_efix_parts.clear();
+  m_efix_parts.reserve(capacity);
+
+  for (usize i = 0; i < capacity; ++i)
+  {
+    m_efix_parts.emplace_back();
+  }
+}
+
+// 002A4E00
+CEditPartsInfo* CEditInfoMngr::GetePartsInfo(ssize index)
+{
+  log_trace("CEditInfoMngr::{}({})", __func__, index);
+
+  if (index < 0 || index >= m_eparts_info.size())
+  {
+    return nullptr;
+  }
+
+  return &m_eparts_info[index];
+}
+
+// 002A4E40
+CEditPartsInfo* CEditInfoMngr::GetePartsInfo(const std::string& parts_name)
+{
+  log_trace("CEditInfoMngr::{}({})", __func__, parts_name);
+
+  for (auto& parts_info : m_eparts_info)
+  {
+    if (parts_name == parts_info.m_name)
+    {
+      return &parts_info;
+    }
+  }
+
+  return nullptr;
+}
+
+// 002A4ED0
+CEditPartsInfo* CEditInfoMngr::GetePartsInfoAtID(ssize id)
+{
+  log_trace("CEditInfoMngr::{}({})", __func__, id);
+
+  for (auto& parts_info : m_eparts_info)
+  {
+    if (id == parts_info.m_id)
+    {
+      return &parts_info;
+    }
+  }
+
+  return nullptr;
+}
+
+// 002A4F40
+CEditPartsInfo* CEditInfoMngr::GetPartsInfoAtType(EEPartsType parts_type)
+{
+  log_trace("CEditInfoMngr::{}({})", __func__, std::to_underlying(parts_type));
+
+  for (auto& parts_info : m_eparts_info)
+  {
+    if (parts_type == parts_info.GetPartsType())
+    {
+      return &parts_info;
+    }
+  }
+
+  return nullptr;
+}
+
+// 002A58E0
+void CEditInfoMngr::LoadEditInfo(char* file, usize file_len)
+{
+  log_trace("CEditInfoMngr::{}({})", __func__, capacity);
+
+  emapInfo = this;
+  emapIdx = 0;
+  emapNowInfo = 0;
+  emapRect = nullptr;
+  emapRectNum = 0;
+  emapRectIdx = 0;
+  emapFixNum = 0;
+  emapFixIdx = 0;
+
+  CScriptInterpreter interpreter{};
+
+  interpreter.SetTag(emap_tag);
+  interpreter.SetScript(file, file_len);
+  interpreter.Run();
+}
--- code/dc2/editmap.h
+++ code/dc2/editmap.h
@@ -1,30 +1,97 @@
 #pragma once
+#include "common/debug.h"
+#include "common/types.h"
+
+#include "dc2/map.h"
 
 struct ePlaceData
 {
-
+public:
 };
 
-class CEditInfoMngr
+struct EP_PLACE_INFO
 {
-  // C
-  ePlaceData* m_place_data{};
+public:
 };
 
-class CEditMap
+enum class EEPartsType
 {
-  // F94
-  CEditInfoMngr m_edit_info{};
+
 };
 
 class CEditPartsInfo
 {
+public:
+  // 001B5630
+  EEPartsType GetPartsType() const;
+
+  // 0
+  ssize m_id{};
+
+  // 3C
+  std::string m_name{};
+
+  // SIZE 0x280
 };
 
 class CEditHouse
 {
+public:
 };
+
+
 
 class CEditParts
 {
+public:
+  struct WallInfo
+  {
+  public:
+  };
+};
+
+class CEditInfoMngr
+{
+public:
+  // 002A4DE0
+  void SetePartsInfoTable(usize capacity);
+  
+  // 002A4DF0
+  void SeteFixPartsTable(usize capacity);
+
+  // 002A4E00
+  CEditPartsInfo* GetePartsInfo(ssize index);
+
+  // 002A4E40
+  CEditPartsInfo* GetePartsInfo(const std::string& parts_name);
+
+  // 002A4ED0
+  CEditPartsInfo* GetePartsInfoAtID(ssize id);
+
+  // 002A4F40
+  CEditPartsInfo* GetPartsInfoAtType(EEPartsType parts_type);
+
+  // 002A58E0
+  void LoadEditInfo(char* file, usize file_len);
+
+  // 0 (length), 4 (buf)
+  std::vector<CEditPartsInfo> m_eparts_info{};
+  // 8 (length), C (buf)
+  std::vector<ePlaceData> m_efix_parts{};
+  // 10
+  unk32 m_unk_field_10{};
+  // 14
+  unk32 m_unk_field_14{};
+};
+
+class CEditMap
+{
+public:
+  struct RemoveInfo
+  {
+  public:
+  };
+
+  // F94
+  CEditInfoMngr m_edit_info{};
 };
