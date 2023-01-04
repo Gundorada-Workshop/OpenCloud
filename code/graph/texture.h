420a333064fa3c0fdfc8d0d508d45d85dce1c648
raSTARgfx <rastargfx@protonmail.com>
2023-01-04 08:53:02 -0500

    move texture to graph project

--- code/graph/texture.h
+++ code/graph/texture.h
@@ -4,7 +4,7 @@
 #include "common/types.h"
 #include "common/strings.h"
 
-namespace host
+namespace graph
 {
   class texture
   {
@@ -59,7 +59,7 @@ namespace host
     }
 
     // gets the debug name
-    inline std::string_view get_debug_name() const
+    inline std::string_view debug_name() const
     {
       return m_debug_name;
     }
@@ -108,21 +108,21 @@ namespace host
 
   protected:
     // settings for the texture
-    create_info m_config;
+    create_info m_config{ };
 
     // debug name for the texture
-    std::string m_debug_name;
+    std::string m_debug_name{ };
   };
 }
 
-IMPLEMENT_ENUM_CLASS_BITWISE_OPERATORS(host::texture::bind_flags);
+IMPLEMENT_ENUM_CLASS_BITWISE_OPERATORS(graph::texture::bind_flags);
 
 namespace std
 {
   template<>
-  struct hash<host::texture::create_info>
+  struct hash<graph::texture::create_info>
   {
-    usize operator()(const host::texture::create_info& config)
+    usize operator()(const graph::texture::create_info& config)
     {
       const u64 width  = static_cast<u64>(config.width);
       const u64 height = static_cast<u64>(config.height);
@@ -141,21 +141,21 @@ namespace std
 namespace fmt
 {
   template<>
-  struct formatter<host::texture::data_format> : formatter<std::string_view>
+  struct formatter<graph::texture::data_format> : formatter<std::string_view>
   {
-    auto format(const host::texture::data_format data_format, format_context& ctx)
+    auto format(const graph::texture::data_format data_format, format_context& ctx)
     {
       switch (data_format)
       {
-      case host::texture::data_format::rgba8:
+      case graph::texture::data_format::rgba8:
         return fmt::format_to(ctx.out(), "{}", "R8G8B8A8");
-      case host::texture::data_format::bgra8:
+      case graph::texture::data_format::bgra8:
         return fmt::format_to(ctx.out(), "{}", "B8G8R8A8");
-      case host::texture::data_format::rgb565:
+      case graph::texture::data_format::rgb565:
         return fmt::format_to(ctx.out(), "{}", "R5G6B5");
-      case host::texture::data_format::r8:
+      case graph::texture::data_format::r8:
         return fmt::format_to(ctx.out(), "{}", "R8");
-      case host::texture::data_format::d16:
+      case graph::texture::data_format::d16:
         return fmt::format_to(ctx.out(), "{}", "D16");
       }
 
@@ -164,10 +164,10 @@ namespace fmt
   };
 
   template<>
-  struct formatter<host::texture::bind_flags> : formatter<std::string_view>
+  struct formatter<graph::texture::bind_flags> : formatter<std::string_view>
   {
     // TODO: this kinda sucks
-    auto format(const host::texture::bind_flags flags, format_context& ctx)
+    auto format(const graph::texture::bind_flags flags, format_context& ctx)
     {
       if (std::to_underlying(flags) == 0)
         return std::format_to(ctx.out(), "flags(none)");
@@ -180,16 +180,16 @@ namespace fmt
       };
 
       std::string flags_string;
-      for (usize i = 0; i < std::to_underlying(host::texture::bind_flags::max); ++i)
+      for (usize i = 0; i < std::to_underlying(graph::texture::bind_flags::max); ++i)
       {
         if (std::to_underlying(flags) & (1 << i))
         {
           flags_string += flag_names[i];
-          flags_string += ",";
+          flags_string += "|";
         }
       }
 
-      // remove trailing comma
+      // remove trailing |
       flags_string.pop_back();
 
       return fmt::format_to(ctx.out(), "flags({})", flags_string);
@@ -197,9 +197,9 @@ namespace fmt
   };
 
   template<>
-  struct formatter<host::texture::create_info> : formatter<std::string_view>
+  struct formatter<graph::texture::create_info> : formatter<std::string_view>
   {
-    auto format(const host::texture::create_info& config, format_context& ctx)
+    auto format(const graph::texture::create_info& config, format_context& ctx)
     {
       return fmt::format_to(ctx.out(), "create_info(fmt: {}, w: {}, h: {}, layers: {}, levels: {}, samples: {})",
         config.format, config.width, config.height, config.layers, config.levels, config.samples);
