60158f2a1f9c50f6c092a4f0eb2d86251729cfd2
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-01-22 01:31:56 +0000

    ADD: Linux version of ALWAYS_INLINE, new ALIGN macro, disable MSVC warning suppression for Linux

--- code/common/macros.h
+++ code/common/macros.h
@@ -1,17 +1,27 @@
 #pragma once
 
-#if defined(_MSC_VER)
 #define WARNING_ID_CONDITIONAL_EXPRESSION_IS_CONSTANT 4127
 #define WARNING_ID_ASSIGNMENT_WITHIN_CONDITIONAL_STATEMENT 4706
 
-#define MSVC_SILENCE_WARNING(id) \
-  __pragma(warning(disable:id))
-
-#define FILE_DISABLE_WARNING(id) MSVC_SILENCE_WARNING(id)
-
-#define ALWAYS_INLINE __forceinline
-#define INLINE inline
+// FILE_DISABLE_WARNING
+#if defined(_MSC_VER)
+  #define MSVC_SILENCE_WARNING(id) \
+    __pragma(warning(disable:id))
+  #define FILE_DISABLE_WARNING(id) MSVC_SILENCE_WARNING(id)
+#elif defined(__GNUG__) || defined(__clang__)
+  #define MSVC_SILENCE_WARNING(id)
+  #define FILE_DISABLE_WARNING(id)
 #endif
+
+// ALWAYS_INLINE
+#if defined(_MSC_VER)
+  #define ALWAYS_INLINE __forceinline
+#elif defined(__GNUG__) || defined(__clang__)
+  #define ALWAYS_INLINE inline __attribute__((__always_inline__))
+#endif
+
+// INLINE
+#define INLINE inline
 
 #define MAYBE_UNUSED [[maybe_unused]]
 #define NO_DISCARD [[nodiscard]]
@@ -19,6 +29,13 @@
 #define FALLTHROUGH [[fallthrough]]
 #define LIKELY [[likely]]
 #define UNLIKELY [[unlikely]]
+
+// ALIGN
+#if defined(_MSC_VER)
+  #define ALIGN(width) __declspec(align(width))
+#elif defined(__GNUG__) || defined(__clang__)
+  #define ALIGN(width) __attribute__((aligned(width)))
+#endif
 
 // Enum class bitwise operators
 #define IMPLEMENT_ENUM_CLASS_BITWISE_OPERATORS(type_)                                                                  \
