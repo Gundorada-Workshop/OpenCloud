a20d3755d292de11f5e04aadb27b54bbbe077401
raSTAR <rastargfx@protonmail.com>
2023-08-12 22:01:35 -0400

    [gen] cleanup

--- generators/gen_scm_version.rb
+++ generators/gen_scm_version.rb
@@ -5,14 +5,15 @@ raise ArgumentError, "Not enough or too many arguments" unless ARGV.count == 3
 
 GIT_ROOT_PATH = ARGV[0]
 
-TEMPLATE_FILE = ARGV[1]
-OUTPUT_FILE   = ARGV[2]
+TEMPLATE_PATH = ARGV[1]
+OUTPUT_PATH  = ARGV[2]
 
 git = Git.open(GIT_ROOT_PATH, :log => Logger.new(STDOUT))
 
-input_file = File.open(TEMPLATE_FILE)
-template = ERB.new(input_file.read)
+File.open(TEMPLATE_PATH, "r") do |input_file|
+  template = ERB.new(input_file.read)
 
-File.open(OUTPUT_FILE, "w") do |f|
-  f.write(template.result(binding))
-end
+  File.open(OUTPUT_PATH, "w") do |output_file|
+    output_file.write(template.result(binding))
+  end
+ end
